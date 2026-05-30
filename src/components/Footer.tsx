7f77aca452e9aa25bf2c0392c4e3878156f7dc7c
monique <moniqueohana@yahoo.com.br>
2026-05-30 01:35:02 -0300

    about me

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 function Footer() {
     return (
-        <footer className="bg-[#BAE6A7] text-zinc-800 py-4 text-center">
+        <footer className="bg-[#BAE6A7] text-zinc-800 py-4 text-center border-t border-gray-300 py-10 px-16 mt-24">
   <p>
     © {new Date().getFullYear()} Najara Limeira. Todos os direitos reservados.
   </p>
 
-  <p className="text-sm mt-2 text-zinc-600">
+  <p className="text-sm mt-2 text-zinc-600 max-w-6xl mx-auto">
     Desenvolvido por <a href="https://github.com/MoniqueOhana">Monique Ohana</a>
   </p>
 </footer>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 
 function Navbar() {
     return (
-        <nav className="w-full bg-[#BAE6A7] shadow-md">
+        <nav className="w-full bg-[#BAE6A7] shadow-md mb-20">
       <div className="max-w-7xl mx-auto px-16 py-7 flex items-center justify-between">
             <img
   src={logo}
@@ -11,7 +11,7 @@ function Navbar() {
   className="w-[180px] object-contain"
 />
 
-            <ul className="flex gap-10 text-[#3f3f3f] text-2xl font-bold-600 font-['Suranna']">
+            <ul className="flex gap-10 text-[#3f3f3f] text-2xl font-bold-600 font-['Cormorant_Garamond']">
                 <li>
             <NavLink
             to="/"
